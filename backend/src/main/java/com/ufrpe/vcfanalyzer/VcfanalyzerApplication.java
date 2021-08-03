855e1cdb3e906a9db395868431b00206aee6ac7d
sealexsandro <sealexsandro@gmail.com>
2021-08-03 18:04:25 -0300

    first commit

---
+++ backend/src/main/java/com/ufrpe/vcfanalyzer/VcfanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ufrpe.vcfanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcfanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcfanalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ufrpe/vcfanalyzer/VcfanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrpe.vcfanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcfanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
